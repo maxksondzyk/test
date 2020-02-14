f304d009dec14bca7bc9dd3d73397961ae110593
Max <maxksiondzyk@gmail.com>
2020-02-14 15:25:14 +0200

    added a bunch to master

--- src/com/ksondzyk/Main.java
+++ src/com/ksondzyk/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         int test = 5;
+        double t = 9;
+        String d = "das"
         System.out.println(test);
     }
 }
