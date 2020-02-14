1a81d3b5fa8e9271e30b07578c7b70e78bd6ac89
Max <maxksiondzyk@gmail.com>
2020-02-14 19:17:17 +0200

    ss

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         int test = 5;
-        double t = 9;
+        double t = 91;
         String d = "das";
         System.out.println(t);
     }
